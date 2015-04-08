fedcaa38a6746c8e66df8993ef00f6a990756729
Bit Connor <bit@mutantlemon.com>
2015-04-08 17:56:10 +0300

    Make some Questionable Tweaks

--- xmonad/xmonad.hs
+++ xmonad/xmonad.hs
@@ -3,22 +3,30 @@ import XMonad.Hooks.DynamicLog
 import XMonad.Hooks.ManageDocks
 import XMonad.Util.Run(spawnPipe)
 import XMonad.Util.EZConfig(additionalKeys)
+import XMonad.Layout.NoBorders(smartBorders)
 import System.IO
+import qualified XMonad.StackSet as W
 
 main = do
     spawn "xsetroot -solid grey12"
     xmproc <- spawnPipe "xmobar"
     xmonad $ defaultConfig
-        { manageHook = manageDocks <+> manageHook defaultConfig
-        , layoutHook = avoidStruts  $  layoutHook defaultConfig
+        { manageHook = manageDocks <+> myManageHook
+        , layoutHook = smartBorders (avoidStruts  $  layoutHook defaultConfig)
         , logHook = dynamicLogWithPP $ xmobarPP
                         { ppOutput = hPutStrLn xmproc
                         , ppTitle = xmobarColor "#ff8800" "" . shorten 60
                         }
         , modMask = mod4Mask     -- Rebind Mod to the Windows key
-        , terminal = "urxvt"
+        , terminal = "instantrun urxvt"
         , normalBorderColor = "#444444"
         , focusedBorderColor = "#ff8800"
         , borderWidth = 2
         }
 
+myManageHook = composeAll . concat $
+    [
+        [ title =? i --> (doF W.focusDown <+> doFloat) | i <- myClassFloats ]
+    ]
+    where
+    myClassFloats = ["termview"]
